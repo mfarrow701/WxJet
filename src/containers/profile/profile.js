51a58b2d7a0e3dddf21f786b178872811ee383c0
Matthew Farrow <matthew.farrow@metoffice.gov.uk>
2018-05-11 09:03:42 +0100

    Update profile

--- src/containers/profile/profile.js
+++ src/containers/profile/profile.js
@@ -1,22 +1,23 @@
 // @flow
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import './profile.css';
 
 class Profile extends Component {
+    name = 'Matthew Farrow';
 
     render() {
         return (
             <div className="Profile">
                 <div className="Avatar">
-                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" title="Avatar" />
+                    <img src={'https://github.com/identicons/' + this.name.replace(' ', '').toUpperCase() + '.png'} alt="Avatar" title="Avatar"/>
                     <h3>Matthew Farrow</h3>
-                    <p>Web Application Developer</p>
+                    <p>Web & hybrid app developer</p>
                 </div>
                 <div className="Details">
-                    <p className="Email"><strong>Email:</strong> N/A</p>
-                    <p className="Favourite-Location"><strong>Home location:</strong> {this.props.selectedLocation.name}</p>
-                    <p><strong>My aircraft:</strong> {this.props.typeIsFixedWing ? 'Fixed' : 'Rotary'} wing</p> 
+                    <p className="Favourite-Location"><strong>Home location:</strong> {this.props.selectedLocation.name}
+                    </p>
+                    <p><strong>My aircraft:</strong> {this.props.typeIsFixedWing ? 'Fixed' : 'Rotary'} wing</p>
                 </div>
             </div>
         )
